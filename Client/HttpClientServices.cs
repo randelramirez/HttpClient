2b856ac9c93b8f65f1672938b952580c31dbb092
Randel Ramirez <randelramirez1@gmail.com>
2021-01-20 21:07:59 +0800

    Refactored DI registration for HttpClient services

---
+++ Client/HttpClientServices.cs
@@ -0,0 +1,74 @@
+﻿using Client.MessageHandlers;
+using Client.TypedClients;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    public static class HttpClientServices
+    {
+        public static IServiceCollection AddHttpClientServices(this IServiceCollection services)
+        {
+            services.ConfigureNamedClient();
+            services.ConfigureClientWithCustomHandler();
+            services.ConfigureNamedClient();
+            
+            return services;
+        }
+
+        private static IServiceCollection ConfigureNamedClient(this IServiceCollection services)
+        {
+            // Using a named client
+            services.AddHttpClient("ContactsClient", client =>
+            {
+                client.BaseAddress = new Uri("https://localhost:44354/");
+                client.Timeout = new TimeSpan(0, 0, 30);
+                client.DefaultRequestHeaders.Clear();
+            })
+            .ConfigurePrimaryHttpMessageHandler(handler => new HttpClientHandler()
+            {
+                AutomaticDecompression = System.Net.DecompressionMethods.GZip
+            });
+
+            return services;
+        }
+
+        private static IServiceCollection ConfigureClientWithCustomHandler(this IServiceCollection services)
+        {
+            services.AddHttpClient("ContactsClientCustomHandler", client =>
+            {
+                client.BaseAddress = new Uri("https://localhost:44354/");
+                client.Timeout = new TimeSpan(0, 0, 30);
+                client.DefaultRequestHeaders.Clear();
+            })
+            // We use 20 seconds at least for this demo so that we can differtiate timeout and cancelled exceptions 
+            // (the httpclient have 30 seconds before timeout and then it throws a cancelled exception)
+            .AddHttpMessageHandler(handler => new TimeOutDelegatingHandler(TimeSpan.FromSeconds(15)))
+            .AddHttpMessageHandler(handler => new RetryPolicyDelegatingHandler(2))
+            .ConfigurePrimaryHttpMessageHandler(handler =>
+                new HttpClientHandler()
+                {
+                    AutomaticDecompression = System.Net.DecompressionMethods.GZip
+                });
+
+            return services;
+        }
+
+        private static IServiceCollection ConfigureTypedClient(this IServiceCollection services)
+        {
+            services.AddHttpClient<ContactsClient>()
+            .ConfigurePrimaryHttpMessageHandler(handler =>
+                 new HttpClientHandler()
+                 {
+                     AutomaticDecompression = System.Net.DecompressionMethods.GZip
+                 });
+
+            return services;
+        }
+    }
+}
--- Client/Program.cs
+++ Client/Program.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Net.Http;
 using System.Threading.Tasks;
-using Client.MessageHandlers;
 using Client.Services;
-using Client.TypedClients;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
@@ -43,6 +40,9 @@ namespace Client
                 builder.AddConsole();
                 builder.AddDebug();
             });
+
+            serviceCollection.AddHttpClientServices();
+
             // register IService on our container with a 
             // scoped lifetime
 
@@ -55,41 +55,7 @@ namespace Client
             // For the HttpClientFactory demos
             //serviceCollection.AddScoped<IService, HttpClientFactoryManagementService>();
 
-            // Using a named client
-            serviceCollection.AddHttpClient("ContactsClient", client =>
-            {
-                client.BaseAddress = new Uri("https://localhost:44354/");
-                client.Timeout = new TimeSpan(0, 0, 30);
-                client.DefaultRequestHeaders.Clear();
-            })
-            .ConfigurePrimaryHttpMessageHandler(handler => new HttpClientHandler()
-            {
-                AutomaticDecompression = System.Net.DecompressionMethods.GZip
-            });
 
-            serviceCollection.AddHttpClient("ContactsClientCustomHandler", client =>
-            {
-                client.BaseAddress = new Uri("https://localhost:44354/");
-                client.Timeout = new TimeSpan(0, 0, 30);
-                client.DefaultRequestHeaders.Clear();
-            })
-            // We use 20 seconds at least for this demo so that we can differtiate timeout and cancelled exceptions 
-            // (the httpclient have 30 seconds before timeout and then it throws a cancelled exception)
-           .AddHttpMessageHandler(handler => new TimeOutDelegatingHandler(TimeSpan.FromSeconds(15)))
-           .AddHttpMessageHandler(handler => new RetryPolicyDelegatingHandler(2))
-           .ConfigurePrimaryHttpMessageHandler(handler =>
-                new HttpClientHandler()
-                {
-                    AutomaticDecompression = System.Net.DecompressionMethods.GZip
-                });
-
-            // using Typed client
-            serviceCollection.AddHttpClient<ContactsClient>()
-            .ConfigurePrimaryHttpMessageHandler(handler =>
-                 new HttpClientHandler()
-                 {
-                     AutomaticDecompression = System.Net.DecompressionMethods.GZip
-                 });
 
             // For the dealing with errors and faults demos
             serviceCollection.AddScoped<IService, ErrorHandlingService>();
