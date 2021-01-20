4f1aed8fe32432a3af5dc51a66ca358058fea18e
Randel Ramirez <randelramirez1@gmail.com>
2021-01-20 21:15:09 +0800

    Code clean up

--- Client/HttpClientServices.cs
+++ Client/HttpClientServices.cs
@@ -2,11 +2,7 @@
 using Client.TypedClients;
 using Microsoft.Extensions.DependencyInjection;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Client
 {
